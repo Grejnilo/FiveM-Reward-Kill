4406a1997597e2e7909b1f120b623d359d126185
Grejnilo <128071227+adeuszx@users.noreply.github.com>
2023-06-01 00:32:34 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,11 +1,11 @@
 ESX = nil
-TriggerEvent('adeuszx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent('GrejRewardKill:SharedObject', function(obj) ESX = obj end)
 
 
-RegisterNetEvent('death:event')
-AddEventHandler('death:event', function()
+RegisterNetEvent('GrejRewardKill:event')
+AddEventHandler('GrejRewardKill:event', function()
     local src = source
     local identifier = ESX.GetPlayerFromId(src).getIdentifier()
-	item = 'paczek' -- nazwa itemu ktory ma byc dawany do ekwipunku po zabojstwie
+	item = 'frytki' -- nazwa itemu ktory ma byc dawany do ekwipunku po zabojstwie
 	identifier.addInventoryItem(item, 3) -- ilosc przedmiotow
 end)
